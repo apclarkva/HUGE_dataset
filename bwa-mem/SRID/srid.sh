727a179f112b925c65592a8c20b54975a774a053
apclarkva <apclarkva@gmail.com>
2018-08-30 16:33:03 -0400

    update output tab files

--- bwa-mem/SRID/srid.sh
+++ bwa-mem/SRID/srid.sh
@@ -1,3 +1,3 @@
 NAME="$1"
 
-python SRID.py -b ../bam-alignments/$NAME.bam -p 12 -r 100 -m 200 -s 71 -n 4 -o ../mobile-elements/out.tab
+python SRID.py -b ../bam-alignments/$NAME.bam -p 12 -r 100 -m 200 -s 71 -n 4 -o ../mobile-elements/$NAME.mobile.tab
