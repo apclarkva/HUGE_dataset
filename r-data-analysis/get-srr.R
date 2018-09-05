12b3a62462bac939b6c0204d6d37ec1ac93becc1
apclarkva <apclarkva@gmail.com>
2018-09-05 10:30:40 -0400

    update to r file

--- r-data-analysis/get-srr.R
+++ r-data-analysis/get-srr.R
@@ -4,6 +4,9 @@
 library(dplyr)
 library(plyr)
 
+closeAllConnections()
+rm(list=ls())
+
 
 allProjects <- read.table("SraRunTable.txt", fill=TRUE)
 allProjects <- allProjects[2:nrow(allProjects),]
@@ -26,13 +29,88 @@ hugeDF <- data.frame("sample" = hugeProjects$V11, "SRR" = hugeProjects$V21)
 
 #candidate B values
 candidateB <- hugeDF[grep("-B-", hugeDF$sample),]
+candidateB$sample <- sub("HUGE-B-", "2011-", candidateB$sample)
+candidateB$sample <- as.Date(sub(".ST", "", candidateB$sample))
 
 #altered DF
 candidateA <- hugeDF[-grep("-B-", hugeDF$sample),]
+candidateA$sample <- sub("HUGE", "2011", candidateA$sample)
+candidateA$sample <- as.Date(sub(".ST", "", candidateA$sample))
+
+#read in measurements from .tab file
+sampleTableA <- data.frame("SRR" = character(0),
+                          "Date" = character(0),
+                          "Candidate" = character(0), 
+                          "Scaffolds" = character(0),
+                          "Split.Site.1" = character(0),
+                          "Split.Site.2" = character(0),
+                          "Supporting.Split.Reads" = character(0),
+                          "Supporting.Read.Pairs.first.in.pair." = character(0))
+sampleTableB <- sampleTableA
+
+
+add_to_table <- function(SRR, date, candidate, sampleTable) {
+  sampleRead <- read.table(paste("mobile-elements/", SRR,".mobile.tab", sep=""), header = TRUE, sep="\t")
+  sampleReadDimensions <- dim(sampleRead) #use first dimensions
+
+  if(sampleReadDimensions[1] == 0) {
+
+    sampleRead <- data.frame("Scaffolds" = c(NA),
+                             "Split.Site.1" = c(NA),
+                             "Split.Site.2" = c(NA),
+                             "Supporting.Split.Reads" = c(NA),
+                             "Supporting.Read.Pairs.first.in.pair." = c(NA))
+    sampleReadDimensions <- c(1,1)
+  }
+
+  allSampleList <- data.frame("SRR" = rep(SRR, sampleReadDimensions[1]),
+                              "Date" = rep(date, sampleReadDimensions[1]),
+                              "Candidate" = rep(candidate, sampleReadDimensions[1])
+  )
+    
+
+  
+  #merge allSampleList with sampleRead
+  allSampleList <- cbind(allSampleList, sampleRead)
+  newSampleTable <- rbind(sampleTable,allSampleList)
+  return(newSampleTable)
+}
 
 
 
+for(row in 1:nrow(candidateA)) {
+  sampleTableA <- add_to_table(as.character(candidateA$SRR[row]), 
+                              as.character(candidateA$sample[row]), 
+                              "A", 
+                              sampleTableA)  
+}
 
+for(row in 1:nrow(candidateB)) {
+  sampleTableB <- add_to_table(as.character(candidateB$SRR[row]), 
+                              as.character(candidateB$sample[row]), 
+                              "B", 
+                              sampleTableB)  
+}
+
+#sampleTable sorted by date
+sampleTableA <- sampleTableA[order(as.Date(sampleTableA$Date)),]
+sampleTableB <- sampleTableB[order(as.Date(sampleTableB$Date)),]
+
+tableA <- sampleTableA[!is.na(sampleTableA$Scaffolds),]
+tableB <- sampleTableB[!is.na(sampleTableB$Scaffolds),]
+
+
+samplesACount <- table(tableA$Date)
+samplesBCount <- table(tableB$Date)
+
+samplesACountOrder <- samplesACount[order(as.Date(names(samplesACount)))]
+samplesBCountOrder <- samplesBCount[order(as.Date(names(samplesBCount)))]
+
+par(mfrow=c(1,2))
+barplot(samplesACountOrder, main="Candidate A",
+        xlab = "Date", ylab = "Count Detected Potential MGEs")
+barplot(samplesBCountOrder, main="Candidate B",
+        xlab = "Date", ylab = "Count Detected Potential MGEs")
 
 
 
